082b4820ecf0113f5248dc6bcd5c06008013f5af
el-crocorino <owenzek@gmail.com>
2016-10-18 13:01:20 +0200

    feature/1-4 - Added simple vars declarations

---
+++ 1-4/first_prog/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main()
+{
+    int userAge(16);
+    string userName("Albert Einstein");
+    int friendsCount(432);
+
+    double pi(3.14159);
+
+    bool isFriend(true);
+
+    char letter('a');
+
+    return 0;
+}
