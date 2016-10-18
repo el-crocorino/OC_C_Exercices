e25c5a0ea8419845e7776adc4b9606c73987e76d
el-crocorino <owenzek@gmail.com>
2016-10-18 13:43:26 +0200

    feature/1-4 - Basic var outputing

--- 1-4/first_prog/main.cpp
+++ 1-4/first_prog/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <string>
 
-
 using namespace std;
+
 int main()
 {
 
-    string userName;
-    int playerCount;
-    bool hasWon;
+    int userAge(16);
+    cout << "L'age de l'utilisateur est: " << userAge << endl;
+
+    int userQI(153);
+    string userName("Albert Einstein");
+
+    cout << "Vous vous appelez " << userName << " et votre QI est de " << userQI << endl;
 
     return 0;
 }
