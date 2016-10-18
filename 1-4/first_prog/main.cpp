11350c03146e0539fb459f9a332298deaab48309
el-crocorino <owenzek@gmail.com>
2016-10-18 13:03:34 +0200

    feature/1-4 - Var declaration whithout initialization

--- 1-4/first_prog/main.cpp
+++ 1-4/first_prog/main.cpp
@@ -3,18 +3,12 @@
 
 
 using namespace std;
-
 int main()
 {
-    int userAge(16);
-    string userName("Albert Einstein");
-    int friendsCount(432);
 
-    double pi(3.14159);
-
-    bool isFriend(true);
-
-    char letter('a');
+    string userName;
+    int playerCount;
+    bool hasWon;
 
     return 0;
 }
